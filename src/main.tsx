2facf00eca1ccd5fb5922ecc4f693b4f5b0c17c5
Vitor Luvizetto <vitor.dluvizetto@gmail.com>
2024-01-12 10:13:00 -0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./theme/main.scss";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <h1>Hello world</h1>
+  </React.StrictMode>
+);
